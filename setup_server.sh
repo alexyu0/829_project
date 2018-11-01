54a0f8415e16fecd34b9145ebe20feede6c71c4e
Ubuntu <ubuntu@ip-172-31-35-238.ec2.internal>
2018-11-01 13:19:59 +0000

    fix setup

--- setup_server.sh
+++ setup_server.sh
@@ -1,11 +1,10 @@
 sudo apt-get -y update
 sudo apt-get -y upgrade
-sudo apt-get -y install \
-	build-essential \
-	lib32z1
+sudo apt-get -y install	build-essential
 
 cd iperf
 ./configure
 sudo make
+sudo apt-get -y install lib32z1
 sudo make install
 
