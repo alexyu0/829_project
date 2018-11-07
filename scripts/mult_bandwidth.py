360be2fac0b5fc72808e5288080a0e01bb5f6907
Zeleena Kearney <zkearney@andrew.cmu.edu>
2018-11-06 23:45:45 -0500

    draft of latency script

--- scripts/mult_bandwidth.py
+++ scripts/mult_bandwidth.py
@@ -14,6 +14,24 @@ TIME_COL = 1
 INFO_NAME = "Info"
 INFO_COL = 6
 
+class Packet():
+	def __init__(self, seqNum, startTime):
+		self.seqNum = seqNum
+		self.acked = False
+		self.minTime = startTime
+		self.maxTime = 0
+
+	def getLatency(self):
+		if not self.acked: # Not acked yet, no latency yet.
+			return -1
+		return self.maxTime - self.minTime
+
+	def updateTime(self, receivedTime):
+		if self.maxTime == 0 or self.maxTime == self.minTime:
+			self.maxTime = receivedTime
+		elif receivedTime > self.maxTime:
+			self.maxTime = receivedTime
+
 # Hardcoded ports to file.
 def getPortsBasedOnFile(file):
 	srcPort = -1
@@ -47,13 +65,13 @@ def parseCSV(file):
 def isBetweenPorts(info, srcPort, destPort):
 	# See if the message is between src > dest.
 	if ("%d  >  %d" % (srcPort, destPort)) in info:
-		return True
+		return True, "s"
 
 	# See if the message is between dest > src.
 	if ("%d  >  %d" % (destPort, srcPort)) in info:
-		return True
+		return True, "d"
 
-	return False
+	return False, "n"
 
 # Determine if data is transferred by checking the info column
 # and looking at the Len of the message sent.
@@ -65,6 +83,16 @@ def isDataTransferred(info):
 			# The amount of data is after the equals sign.
 			return True, int(lenInfo[1])
 	return False, 0
+
+# Gets the sequence number.
+def getInfoField(info, field):
+	for infoField in info.split(" "):
+		if field in infoField:
+			seqInfo = infoField.split("=")
+			# The amount of data is after the equals sign.
+			return int(seqInfo[1])
+	return -1 # Should never get here.
+
 
 
 def calculateBandwidth(csvData, srcPort, destPort):
@@ -90,12 +118,106 @@ def calculateBandwidth(csvData, srcPort, destPort):
 			dataForBucket = 0  # Reset y-axis sum.
 
 		# Sum the amount of data transferred, if it was in this row.
-		if isBetweenPorts(row[INFO_COL], srcPort, destPort):
+		is_btwn_ports, from_which = isBetweenPorts(row[INFO_COL], srcPort, destPort)
+		if is_btwn_ports:
 			isData, dataLen = isDataTransferred(row[INFO_COL])
 			if isData:
 				dataForBucket += dataLen  # Length of data transferred.
 
 	return buckets, dataPerBucketList
+
+
+# With the given map of packets, get the latencies per packet.
+def convertMapToLatency(buckets, dataPerBucket, packets):
+	for key in packets:
+		pkt = packets[key]
+		latency = pkt.getLatency()
+		if latency != -1:
+			index = 0
+			while index < len(buckets) and buckets[index] < latency:
+				index += 1
+			# At the index to add a count for this packet.
+			if index != len(buckets):
+				dataPerBucket[index] += 1
+
+
+
+# Sum up the information in the maps.
+def calculatePacketsLatency(srcPackets, destPackets):
+
+	# Change x-axis here!
+	numElements = 20
+	buckets = np.linspace(0.0, 2.0, num=numElements, endpoint=True)
+	dataPerBucketList = np.zeros(numElements)
+
+	convertMapToLatency(buckets, dataPerBucketList, srcPackets)
+	convertMapToLatency(buckets, dataPerBucketList, destPackets)
+
+	return buckets, dataPerBucketList
+
+
+# Get the tail latency by keeping track of individual packets,
+# find when they are received (or ACK'd), and note that latency.
+# Group latency values into buckets to get tail latency (x-axis)
+# vs. number of packets (y-axis).
+def calculateTailLatency(csvData, srcPort, destPort):
+	srcPackets = dict() # Keys are SeqNums sent from the srcPort.
+	destPackets = dict() # Keys are SeqNums sent from the destPort.
+	
+
+	# print("Not implemented error")
+	# # TODO: remove when implemented
+	# pass
+
+	print("Calculating tail latency between port %d and port %d..." % (srcPort, destPort))
+
+	bucketI = 0
+	bucketStart = 0
+	nextBucket = bucketStart + bucketSize
+
+	buckets = []
+	dataPerBucketList = []
+	dataForBucket = 0
+
+	# Ignore title row (row 0).
+	for i in range(1, len(csvData)):
+		row = csvData[i]
+		info = row[INFO_COL]
+
+		is_btwn_ports, from_which = isBetweenPorts(row[INFO_COL], srcPort, destPort)
+		if is_btwn_ports:
+			seqNum = int(getInfoField(info, "Seq"))
+			ackNum = int(getInfoField(info, "Ack"))
+			#receivedTime = int(getInfoField(info, "TSval"))
+			receivedTime = float(row[TIME_COL])
+
+			# Depending on who sent it, update the dictionaries of packets.
+			if from_which == "s":  # Source sent to dest.
+				# Check if this source packet exists, if not then add it.
+				if seqNum not in srcPackets:
+					srcPackets[seqNum] = Packet(seqNum, receivedTime)
+				# Check if this is acknowledging a dest packet.
+				if ackNum in destPackets:
+					pkt = destPackets[ackNum]
+					pkt.acked = True
+					pkt.updateTime(receivedTime)
+
+			elif from_which == "d":  # Dest sent to source.
+				# Check if this dest packet exists, if not then add it.
+				if seqNum not in destPackets:
+					destPackets[seqNum] = Packet(seqNum, receivedTime)
+				# Check if this is acknowledging a src packet.
+				if ackNum in srcPackets:
+					pkt = srcPackets[ackNum]
+					pkt.acked = True
+					pkt.updateTime(receivedTime)
+			
+	buckets, dataPerBucketList = calculatePacketsLatency(srcPackets, destPackets)
+
+	return buckets, dataPerBucketList
+
+
+
 
 # To get bandwidth, divide the data transferred in that bucket time range
 # by the size of that time range.
@@ -112,24 +234,33 @@ def divideByBuckets(buckets, dataPerBucketList, prefix):
 	return bandwidthPerBucket
 
 # Plot.
-def plotBandwidth(minBucket, maxBucket, results, prefix, files):
+def plotBandwidth(minBucket, maxBucket, results, prefix, files, metric):
 	xaxis = np.arange(minBucket, maxBucket, bucketSize)
 
 	if len(results) == 0:
 		plt.plot(xaxis[:len(results[0])], results[0])
-		plt.title("Bandwidth vs. Time for file %s" % files[0])
+		if metric == "bw":
+			plt.title("Bandwidth vs. Time for file %s" % files[0])
+		elif metric == "latency":
+			plt.title("Per Packet Latency for file %s" % files[0])
 	else:
 		for i in range(len(results)):
 			plt.plot(xaxis[:len(results[i])], results[i], label=files[i].split("/")[-1])
-			plt.title("Bandwidth vs. Time")
+			if metric == "bw":
+				plt.title("Bandwidth vs. Time")
+			elif metric == "latency":
+				plt.title("Per Packet Latency")
 	plt.xlabel("Time (in buckets of %0.2f seconds)" % bucketSize)
-	plt.ylabel("Bandwidth (in data %sB/second)" % prefix)
+	if metric == "bw":
+		plt.ylabel("Bandwidth (in data %sB/second)" % prefix)
+	elif metric == "latency":
+		plt.ylabel("Latency (in seconds)")
 	plt.legend()
 	plt.show()
 
 
 # Main.
-def getBandwidth():
+def main(metric):
 	if len(sys.argv) <= 1:
 		print("Please give at least one file as an argument.")
 		exit()
@@ -151,22 +282,30 @@ def getBandwidth():
 		csvData = parseCSV(file)
 		print("Got CSV data, row 1 = ", csvData[1])
 
-		buckets, dataPerBucketList = calculateBandwidth(csvData, srcPort, destPort)
+		if metric == "bw":
+			buckets, dataPerBucketList = calculateBandwidth(csvData, srcPort, destPort)
+
+			bandwidthPerBucket = divideByBuckets(buckets, dataPerBucketList, prefix)
+
+			print("Bandwidth per bucket = ", bandwidthPerBucket)
+			results.append(bandwidthPerBucket)
+
+		elif metric == "latency":
+			buckets, dataPerBucketList = calculateTailLatency(csvData, srcPort, destPort)
+
+			results.append(dataPerBucketList)
+
 		if buckets[-1] > maxBucket:
 			maxBucket = buckets[-1] #+ bucketSize  # Include max (last num is excluded).
 
 		print("Buckets = ", buckets)
 		print("Data per bucket = ", dataPerBucketList)
 
-		bandwidthPerBucket = divideByBuckets(buckets, dataPerBucketList, prefix)
-
-		print("Bandwidth per bucket = ", bandwidthPerBucket)
-		results.append(bandwidthPerBucket)
 
 		print("Calculations on file %s complete." % file)
 
 	print("Plotting all files...")
-	plotBandwidth(minBucket, maxBucket, results, prefix, files)
+	plotBandwidth(minBucket, maxBucket, results, prefix, files, metric)
 	print("Plotting complete.")
 
 	
@@ -182,4 +321,6 @@ def getBandwidth():
 # Parse command-line arguments.
 # args = parse_arguments()
 # bucketSize = args.bucketSize
-getBandwidth()
+
+
+main("latency")
