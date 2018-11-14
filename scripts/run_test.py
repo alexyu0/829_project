49e8c0a9084dd30a0059fa0c85cc5530867ee581
Alex Yu <alexyu0925@gmail.com>
2018-11-14 14:22:05 -0500

    finally script works

--- scripts/run_test.py
+++ scripts/run_test.py
@@ -3,30 +3,28 @@ import time
 import io
 import sys
 import argparse
-import threading
 import subprocess
 import time
+import random
 import paramiko
+import atexit
 
 from constants import PORT_START
 
-class StoppableThread(threading.Thread):
-    """Thread class with a stop() method. The thread itself has to check
-    regularly for the stopped() condition."""
 
-    def __init__(self, group=None, target=None, name=None, verbose=None, args=(), kwargs=None):
-        super().__init__()
-        self._stop_event = threading.Event()
-        self._target = target
-        self._args = args
+def ssh_client_connect(ip, keyfile):
+    ssh_client = paramiko.client.SSHClient()
+    ssh_client.load_system_host_keys()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh_client.connect(ip,
+        username="ubuntu",
+        key_filename=keyfile)
+    ch = ssh_client.get_transport().open_session()
+    ch.get_pty()
+    ch.set_combine_stderr(True)
+    return (ssh_client, ch)
 
-    def stop(self):
-        self._stop_event.set()
-
-    def stopped(self):
-        return self._stop_event.is_set()
-
-def server_iperf_worker(ips, test_dir, i, args):
+def server_iperf(ips, test_dir, i, file_name, args):
     """
     starts iperf in server mode on each of the AWS instances defined by an
     entry in ips
@@ -34,26 +32,18 @@ def server_iperf_worker(ips, test_dir, i, args):
     ssh_info = []
     filenames = []
     for j in range(0, len(ips)):
-        ssh_client = paramiko.client.SSHClient()
-        ssh_client.load_system_host_keys()
-        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh_client.connect(ips[j], 
-            username="ubuntu",
-            key_filename=args.keyfile)
-        ch = ssh_client.get_transport().open_session()
-        ch.get_pty()
-        ch.set_combine_stderr(True)
-        filename = "IPERFDUMP_{}_server{}_{}".format(args.fname, j+1, i)
+        ssh_client, ch = ssh_client_connect(ips[j], args.keyfile)
+        filename = "STDOUTDUMP_{}_server{}_{}".format(file_name, j+1, i)
         cmd = "iperf3 -s > {}_{}".format(
             os.path.basename(test_dir), filename)
         ch.exec_command(cmd)
         print("server iperf for {} started".format(ips[j]))
         filenames.append(filename)
         ssh_info.append((ssh_client, ch))
+    return (ssh_info, filenames)
 
+def kill_server_iperf(ssh_info, filenames, test_dir):
     # wait to finish, then scp dumps
-    while not threading.current_thread().stopped():
-        time.sleep(0.5)
     for j in range(0, len(ssh_info)):
         (ssh_client, ch) = ssh_info[j]
         ch.close()
@@ -64,79 +54,97 @@ def server_iperf_worker(ips, test_dir, i, args):
         ssh_client.exec_command("rm {}_{}".format(os.path.basename(test_dir), filenames[j]))
         ssh_client.close()
 
-def tcpdump_worker(ips, is_client, test_dir, i, args):
+def server_tcpdump(ips, test_dir, i, file_name, file_name_short, args):
     """
-    starts tcpdump at the endpoint specified by is_client
+    starts tcpdump at the server endpoint
     """
-    if is_client:
-        # client
-        client_tcpdump_procs = []
-        filenames = []
-        for j in range(0, len(ips)):
-            fname = "{}/{}_client{}_{}.pcap".format(test_dir, args.fname, j+1, i)
-            cmd = "sudo tcpdump -i any port {} -w {}".format(
-                PORT_START + j,
-                fname)
-            client_tcpdump_procs.append(subprocess.Popen([cmd],
-                shell=True))
-            filenames.append(fname)
+    ssh_info = []
+    filenames = []
+    for j in range(0, len(ips)):
+        time.sleep(1)
+        ssh_client, ch = ssh_client_connect(ips[j], args.keyfile)
+        fname = "{}_server{}_{}".format(file_name, j+1, i)
+        cmd = "sudo tcpdump tcp -n -B 4096 port 5201 -w {}_{}.pcap".format(
+            os.path.basename(test_dir), 
+            fname)
+        ch.exec_command(cmd)
+        filenames.append(fname)
+        ssh_info.append((ssh_client, ch))
+    return (ssh_info, filenames)
 
-        # wait to finish, then kill tcpdump
-        while not threading.current_thread().stopped():
-            time.sleep(0.5)
-        for j in range(0, len(client_tcpdump_procs)):
-            os.system("pgrep tcpdump | xargs sudo kill -SIGTERM")
-            time.sleep(0.5)
-            subprocess.run("zstd --rm -19 -f {} -o {}.zst".format(
-                filenames[j], filenames[j]),
-                shell=True,
-                check=True)
-    else:
-        # server
-        ssh_info = []
-        filenames = []
-        for j in range(0, len(ips)):
-            ssh_client = paramiko.client.SSHClient()
-            ssh_client.load_system_host_keys()
-            ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            ssh_client.connect(ips[j], 
-                username="ubuntu",
-                key_filename=args.keyfile)
-            ch = ssh_client.get_transport().open_session()
-            ch.get_pty()
-            ch.set_combine_stderr(True)
-            fname = "{}_server{}_{}".format(args.fname, j+1, i)
-            cmd = "sudo tcpdump port 5201 -w {}_{}.pcap".format(
+def kill_server_tcpdump(ssh_info, filenames, test_dir):
+    # wait to finish, then scp tcp dumps
+    for j in range(0, len(ssh_info)):
+        (ssh_client, ch) = ssh_info[j]
+        ch.close()
+        ssh_client.exec_command("pgrep tcpdump | xargs sudo kill -SIGINT")
+        stdin, stdout, stderr = ssh_client.exec_command(
+            "zstd --rm -19 -f {}_{}.pcap -o {}_{}.pcap.zst".format(
                 os.path.basename(test_dir), 
-                fname)
-            ch.exec_command(cmd)
-            filenames.append(fname)
-            ssh_info.append((ssh_client, ch))
-
-        # wait to finish, then scp tcp dumps
-        while not threading.current_thread().stopped():
-            time.sleep(0.5)
-        for j in range(0, len(ssh_info)):
-            (ssh_client, ch) = ssh_info[j]
-            ch.close()
-            ssh_client.exec_command("pgrep tcpdump | xargs sudo kill -SIGTERM")
-            stdin, stdout, stderr = ssh_client.exec_command(
-                "zstd --rm -19 -f {}_{}.pcap -o {}_{}.pcap.zst".format(
-                    os.path.basename(test_dir), 
-                    filenames[j],
-                    os.path.basename(test_dir), 
-                    filenames[j]))
-            if stdout.channel.recv_exit_status() != 0:
-                print(stderr)
-                sys.exit(1)
-            ftp_client = ssh_client.open_sftp()
-            ftp_client.get(
-                "{}_{}.pcap.zst".format(os.path.basename(test_dir), filenames[j]),
-                "{}/{}.pcap.zst".format(test_dir, filenames[j]))
-            ssh_client.exec_command("rm {}_{}.pcap.zst".format(
+                filenames[j],
                 os.path.basename(test_dir), 
                 filenames[j]))
-            ssh_client.close()
+        if stdout.channel.recv_exit_status() != 0:
+            print(stderr)
+            print("hi")
+            sys.exit(1)
+        ftp_client = ssh_client.open_sftp()
+        ftp_client.get(
+            "{}_{}.pcap.zst".format(os.path.basename(test_dir), filenames[j]),
+            "{}/{}.pcap.zst".format(test_dir, filenames[j]))
+        ssh_client.exec_command("rm {}_{}.pcap.zst".format(
+            os.path.basename(test_dir), 
+            filenames[j]))
+        ssh_client.close()
+
+def client_tcpdump(ips, test_dir, i, file_name, file_name_short, args):
+    """
+    starts tcpdump at the client endpoint
+    """
+    client_tcpdump_procs = []
+    filenames = []
+    for j in range(0, len(ips)):
+        time.sleep(1)
+        if args.longshort and j == 1:
+            fname = "{}/{}_client{}_{}.pcap".format(test_dir, file_name_short, j+1, i)
+        else:
+            fname = "{}/{}_client{}_{}.pcap".format(test_dir, file_name, j+1, i)
+        cmd = "sudo tcpdump tcp -n -i any -B 4096 port {} and dst host {} -w {}".format(
+            PORT_START + j,
+            ips[j],
+            fname)
+        client_tcpdump_procs.append(subprocess.Popen([cmd],
+            shell=True))
+        filenames.append(fname)
+    return (client_tcpdump_procs, filenames)
+
+def kill_client_tcpdump(client_tcpdump_procs, filenames, test_dir):
+    os.system("pgrep tcpdump | xargs sudo kill -SIGINT")
+    remaning_tcpdump_procs = subprocess.run("pgrep tcpdump",
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE)
+    if len(remaning_tcpdump_procs.stdout.decode("utf-8").strip()) != 0:
+        print("WARNING WARNING")
+    for j in range(0, len(client_tcpdump_procs)):
+        subprocess.run("zstd --rm -19 -f {} -o {}.zst".format(
+            filenames[j], filenames[j]),
+            shell=True,
+            check=True)
+        print("client {} compressed".format(j))
+
+    for i in range(0, 2):
+        # spam for good measure
+        os.system("pgrep tcpdump | xargs sudo kill -SIGINT")
+
+def cleanup(ips, keyfile):
+    os.system("pgrep iperf3 | xargs sudo kill -9")
+    os.system("pgrep tcpdump | xargs sudo kill -9")
+    for ip in ips:
+        ssh_client, ch = ssh_client_connect(ip, keyfile)
+        ssh_client.exec_command("pgrep iperf3 | xargs sudo kill -9")
+        ssh_client.exec_command("pgrep tcpdump | xargs sudo kill -9")
+        ssh_client.close()
 
 def test(args):
     """
@@ -145,18 +153,37 @@ def test(args):
     # set up hosts
     ips = []
     with open(args.ips) as f:
-        ips = f.read().split("\n")
+        ips = f.read().strip().split("\n")
+        ips = [ip for ip in ips if len(ip) > 0]
     print("...hosts set up")
+
+    # make file name
+    duration = ""
+    short_duration = ""
+    if args.time is not None:
+        duration = "-t {}".format(args.time)
+        name_duration = "{}s".format(str(args.time))
+        short_duration = "-t {}".format(int(int(args.time) * 0.01))
+        name_short_duration = "{}s".format(str(int(int(args.time) * 0.01)))
+    elif args.size is not None:
+        duration = "--bytes {}".format(int(args.size) * 1000000)
+        name_duration = "{}s".format(str(int(args.size) * 1000000))
+        short_duration = "--bytes {}".format(int(int(args.size) * 10000))
+        name_short_duration = "{}MB".format(str(int(int(args.size) * 10000)))
+    file_name = "{}_{}".format(args.location, name_duration)
+    file_name_short = ""
+    if args.longshort:
+        file_name_short = "{}_{}".format(args.location, name_short_duration)
 
     # set up test directory
     args.path = args.path.rstrip("/")
     root_test_dir = os.path.abspath(args.path)
     if args.concurrentlong:
-        test_dir = "{}/{}".format(root_test_dir, "concurrent_long")
+        test_dir = "{}/{}/{}".format(root_test_dir, "concurrent_long", file_name)
     elif args.longshort:
-        test_dir = "{}/{}".format(root_test_dir, "long_and_short")
+        test_dir = "{}/{}/{}".format(root_test_dir, "long_and_short", file_name)
     elif args.normal:
-        test_dir = "{}/{}".format(root_test_dir, "normal")
+        test_dir = "{}/{}/{}".format(root_test_dir, "normal", file_name)
         ips = [ips[0]] # only 1 server
     else:
         print("NO VALID TEST STYLE")
@@ -164,66 +191,77 @@ def test(args):
     if not os.path.exists(test_dir):
         os.makedirs(test_dir)
     print("...test_dir {} set up".format(test_dir))
+    atexit.register(cleanup, ips=ips, keyfile=args.keyfile)
+    print("...cleanup registered")
     
     for i in range(1, int(args.numtests) + 1):
         print("\n\n STARTING TEST {}...".format(i))
         # local tcpdump 
-        client_tcpdump_thread = StoppableThread(target=tcpdump_worker, 
-            args=(ips, True, test_dir, i, args))
-        client_tcpdump_thread.start()
-        print("...started local tcpdump thread")
+        client_tcpdump_procs, c_t_filenames = client_tcpdump(
+            ips, test_dir, i, file_name, file_name_short, args)
+        print("...started local tcpdump")
         if (i == 1):
             time.sleep(10) # give you time to enter password
 
         # remote ssh start iperf server
-        server_iperf_thread = StoppableThread(target=server_iperf_worker, 
-            args=(ips, test_dir, i, args))
-        server_iperf_thread.start()
-        print("...started server iperf thread")
+        iperf_ssh_info, s_i_filenames = server_iperf(
+            ips, test_dir, i, file_name, args)
+        print("...started server iperf")
 
         # remote ssh tcpdump server side
-        server_tcpdump_thread = StoppableThread(target=tcpdump_worker, 
-            args=(ips, False, test_dir, i, args))
-        server_tcpdump_thread.start()
-        print("...started server tcpdump thread")
+        tcpdump_ssh_info, s_t_filenames = server_tcpdump(
+            ips, test_dir, i, file_name, file_name_short, args)
+        print("...started server tcpdump")
 
         # local iperf client start
         time.sleep(5) # let server get ready
         print("starting local iperf...")
-        duration = ""
-        if args.time is not None:
-            duration = "-t {}".format(args.time)
-        elif args.size is not None:
-            duration = "--bytes {}".format(args.size)
-
         writers = []
         client_iperf_procs = []
         for j in range(0, len(ips)):
-            w = io.open("{}/IPERFDUMP_{}_client{}_{}".format(test_dir, args.fname, j+1, i), "wb")
+            w = io.open("{}/STDOUTDUMP_{}_client{}_{}".format(test_dir, file_name, j+1, i), "wb")
             writers.append(w)
-            cmd = "iperf3 --reverse --cport {} {} -c {}".format(
-                PORT_START + j, 
-                duration,
-                ips[j])
+            if args.longshort and j == 1:
+                cmd = "iperf3 --reverse --cport {} {} -c {}".format(
+                    PORT_START + j, 
+                    short_duration,
+                    ips[j])
+            else:
+                cmd = "iperf3 --reverse --cport {} {} -c {}".format(
+                    PORT_START + j, 
+                    duration,
+                    ips[j])
             client_iperf_procs.append(subprocess.Popen(cmd,
                 shell=True,
                 stdout=w))
 
         # wait to finish
-        for j in range(0, len(client_iperf_procs)):
-            proc = client_iperf_procs[j]
+        if args.longshort:
+            proc = client_iperf_procs[0]
             while proc.poll() is None:
-                time.sleep(0.5)
-            writers[j].close()
+                client_iperf_procs[1].wait()
+                print("short client iperf done, going again")
+                time.sleep(random.randint(5, 10))
+                cmd = "iperf3 --reverse --cport {} {} -c {}".format(
+                    PORT_START + 1, 
+                    short_duration,
+                    ips[j])
+                client_iperf_procs[1] = subprocess.Popen(cmd,
+                    shell=True,
+                    stdout=w)
+        else:
+            for j in range(0, len(client_iperf_procs)):
+                proc = client_iperf_procs[j]
+                while proc.poll() is None:
+                    time.sleep(0.5)
+                writers[j].close()
             
-        # kill all threads
-        client_tcpdump_thread.stop()
-        server_tcpdump_thread.stop()
-        server_iperf_thread.stop()
-        client_tcpdump_thread.join()
-        server_tcpdump_thread.join()
-        server_iperf_thread.join()
+        # kill all other stuff
+        kill_client_tcpdump(client_tcpdump_procs, c_t_filenames, test_dir)
+        kill_server_tcpdump(tcpdump_ssh_info, s_t_filenames, test_dir)
+        kill_server_iperf(iperf_ssh_info, s_i_filenames, test_dir)
         print("...test {} done".format(i))
+        time.sleep(1)
 
     return
 
@@ -234,8 +272,8 @@ parser = argparse.ArgumentParser(prog="PROG",
     formatter_class=formatter)
 
 required_args = parser.add_argument_group("required")
-required_args.add_argument("-f", "--fname",
-    help="file name for trace dump, format <location>_<duration or size of test>\n")
+required_args.add_argument("-l", "--location",
+    help="location trace is taken in, used for file name\n")
 required_args.add_argument("-I", "--ips",
     help="file containing list of server IPs, \n*** DON'T INCLUDE ENDPOINT OR TRIAL # ***\n")
 required_args.add_argument("-P", "--path", 
@@ -247,9 +285,9 @@ required_args.add_argument("-i", "--keyfile",
 
 test_args = parser.add_mutually_exclusive_group(required=True)
 test_args.add_argument("-t", "--time",
-    help="amount of time to run test for\n")
+    help="amount of time to run test for, in seconds\n")
 test_args.add_argument("-s", "--size",
-    help="size of data to be transmitted\n")
+    help="size of data to be transmitted, in MB\n")
 
 test_style_args = parser.add_mutually_exclusive_group(required=True)
 test_style_args.add_argument("-C", "--concurrentlong",
@@ -265,5 +303,6 @@ test_style_args.add_argument("-N", "--normal",
 parser.set_defaults(func=test)
 
 if __name__ == '__main__':
+    random.seed()
     args = parser.parse_args()
     args.func(args)
