b19ea7be5b3295b995741f9b41256bb3bd5393a5
Zeleena Kearney <zkearney@andrew.cmu.edu>
2018-11-18 20:24:18 -0500

    adapting latency with helper function

--- scripts/latency.py
+++ scripts/latency.py
@@ -4,11 +4,23 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
+from helpers import group_files
+
 
 # Default values.
 bucketSize = 0.000005
 minBucket = 0.0
 maxBucket = 0.0009
+
+# removes csv file after extracting csv data
+def parseCSV(file, save):
+    csvData = []
+    with open(file, newline='') as csvfile:
+        csvReader = csv.reader(csvfile)
+        for row in csvReader:
+            csvData.append(row)
+    csvfile.close()
+    return csvData
 
 
 # Plot possibly multiple files for latency.
@@ -51,8 +63,16 @@ def sortIntoBuckets(buckets, csvData):
 				countsInBuckets[i] += 1
 	return countsInBuckets
 
+# For each bucket, divide by the number of trials.
+def averageOverTrials(buckets, num_trials):
+	pass
+
+
 # Main.
 def getLatency(csvDataForFiles, graphDir):
+	print("Running latency script...")
+
+	print(csvDataForFiles)
 
 	# 2D array of results. Indexing into results gives the data for the y-axis.
 	results = []
@@ -61,34 +81,48 @@ def getLatency(csvDataForFiles, graphDir):
 	numBuckets = int((maxBucket - minBucket) / float(bucketSize))
 	buckets = np.linspace(minBucket, maxBucket, num=numBuckets, endpoint=False)
 
-	for filename in csvDataForFiles:
+	# key is a run, each run's list should be averaged
+	runs = group_files(csvDataForFiles, False)
 
-		# partition into client or server
+	print(runs)
 
+	for endpoint_no in runs:
+		trials = runs[endpoint_no]
+		num_trials = len(trials)
 
+		for t in range(num_trials):
 
-
-		file = sys.argv[argNum]
-		files.append(file)
-		print("Getting bandwidth calculations on file %s wtih bucket size %0.3f..." % (file, bucketSize))
-
-		csvData = parseCSV(file)
-		print("Got CSV data, row 1 = ", csvData[1])
-
-		# Get the tail latency by keeping track of individual packets,
-		# find when they are received (or ACK'd), and note that latency.
-		# Group latency values into buckets to get tail latency (x-axis)
-		# vs. number of packets (y-axis).
-
-		countsInBuckets = sortIntoBuckets(buckets, csvData)
-
-		results.append(countsInBuckets)
-
-		print("Calculations on file %s complete." % file)
-
-	print("Plotting all files...")
-	plotLatency(buckets, results, files, graphDir)
-	print("Plotting complete.")
+			csvData = trials[t]
 
 
-#getLatency()
+			#file = sys.argv[argNum]
+			#files.append(file)
+			print("Getting latency calculations for endpoint %s trial %d with bucket size %0.3f..." % (endpoint_no, t, bucketSize))
+
+			#csvData = parseCSV(file)
+			#print("Got CSV data, row 1 = ", csvData[1])
+
+			# Get the tail latency by keeping track of individual packets,
+			# find when they are received (or ACK'd), and note that latency.
+			# Group latency values into buckets to get tail latency (x-axis)
+			# vs. number of packets (y-axis).
+
+			countsInBuckets = sortIntoBuckets(buckets, csvData)
+
+			results.append(countsInBuckets)
+
+			#print("Calculations for that trial complete." % file)
+
+		#print("All trials for endpoint %s gathered. Averaging over %d trials..." %(endpoint_no, num_trials))
+		#buckets = averageOverTrials(buckets, num_trials)
+
+		print("Plotting endpoint %s..." % endpoint_no)
+		plotLatency(buckets, results, files, graphDir)
+
+	print("Plotting complete for all runs.")
+
+
+CSV_DATA = parseCSV("tmp.csv", False)
+CSV_DATA_FOR_FILES = {}
+CSV_DATA_FOR_FILES["tmp.csv"] = CSV_DATA
+getLatency(CSV_DATA_FOR_FILES, "tmp")
